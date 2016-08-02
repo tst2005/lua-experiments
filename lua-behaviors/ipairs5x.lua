f7b0b8583a6b2574339bbed3aefcebb7c75de3fa
TsT <tst2005@gmail.com>
2016-08-03 00:01:12 +0200

    improvement

--- lua-behaviors/ipairs5x.lua
+++ lua-behaviors/ipairs5x.lua
@@ -4,25 +4,39 @@
 
 -- This discussion seems to pertain to Lua 5.1. In Lua 5.2, the __pairs and __ipairs metamethods were added. -- Anders Petersson
 
+local function is_not_callable(t)
+	return not (getmetatable(t) or {}).__call
+end
 
-local function ipairs53custom(t, startvalue) -- lua5.3 does NOT use mt.__ipairs
+local function ipairs53custom(t, startvalue, callabletable) -- lua5.3 does NOT use mt.__ipairs
 	local i = startvalue and type(startvalue)=="number" and startvalue-1 or 0
 	local mt = getmetatable(t)
 	local custom = mt and mt.__index
 	if not custom then
 		custom = function(_,k) return t[k] end
 	else
-		if type(custom)=="table" then
+		if type(custom)=="table" and (not callabletable or is_not_callable(custom)) then
 			local c = custom
 			custom = function(_,k) return c[k] end
-		elseif type(custom)~="function" then
-			error("invalid")
-			--custom = function(_,k) return t[k] end
 		end
 	end
+	--local custom=custom
 	return function()
 		i = i + 1
 		local v = custom(t,i)
+		if v ~= nil then
+			return i, v
+		end
+	end
+end
+
+local function ipairs53withstart(t, startvalue) -- lua5.3 like (does NOT use mt.__ipairs) but with start value support
+	local i = startvalue and type(startvalue)=="number" and startvalue-1 or 0
+	local mt = getmetatable(t)
+	t = mt and type(mt.__index)=="table" and mt.__index or t
+	return function()
+		i = i + 1
+		local v = t[i]
 		if v ~= nil then
 			return i, v
 		end
@@ -33,10 +47,9 @@ local function ipairs53(t) -- lua5.3 does NOT use mt.__ipairs
 	local i = 0
 	local mt = getmetatable(t)
 	t = mt and type(mt.__index)=="table" and mt.__index or t
-	local tget = function(t,k) return t[k] end
 	return function()
 		i = i + 1
-		local v = tget(t,i)
+		local v = t[i]
 		if v ~= nil then
 			return i, v
 		end
@@ -78,6 +91,7 @@ local M = {}
 M.ipairs51 = ipairs51
 M.ipairs52 = ipairs52
 M.ipairs53 = ipairs53
+M.ipairs53withstart = ipairs53withstart
 M.ipairs53custom = ipairs53custom
 M.native = _G.ipairs
 
--- lua-behaviors/pairs5x.lua
+++ lua-behaviors/pairs5x.lua
@@ -4,11 +4,11 @@
 
 -- This discussion seems to pertain to Lua 5.1. In Lua 5.2, the __pairs and __ipairs metamethods were added. -- Anders Petersson 
 
-local function pairs51(t) -- lua5.1 does NOT use mt.__pairs
+local function pairs51simple(t) -- lua5.1 does NOT use mt.__pairs
 	return next, t, nil
 end
 
-local function pairs53(t) -- lua5.2 and lua5.3 use mt.__pairs
+local function pairs53simple(t) -- lua5.2 and lua5.3 use mt.__pairs
 	local mt = getmetatable(t)
 	if mt and mt.__pairs then
 		return mt.__pairs(t)
@@ -16,13 +16,63 @@ local function pairs53(t) -- lua5.2 and lua5.3 use mt.__pairs
 	return next, t, nil
 end
 
-local pairs52 = pairs53
+local pairs52simple = pairs53simple
+
+--- shadow iterator ---
+
+-- use pairs, but never expose the table `t` or the function `next`
+local function pairs51shadow(t)
+	local function f(_, k)
+		return next(t, k)
+	end
+	return f, "shadow", nil
+end
+
+-- same than shadow pairs, byt also deny the way to call the handler with other key value
+-- each call return the next result
+local function pairs51shadowest(t)
+	local k
+	local function f()
+		local k2,v2 = next(t, k)
+		k=k2
+		return k2, v2
+	end
+	return f, "shadowest", nil
+end
+
+local function pairs53shadow(t)
+	local mt = getmetatable(t)
+	if mt and mt.__pairs then
+		return mt.__pairs(t)
+	end
+	return pairs51shadow(t)
+end
+local function pairs53shadowest(t)
+	local mt = getmetatable(t)
+	if mt and mt.__pairs then
+		return mt.__pairs(t)
+	end
+	return pairs51shadowest(t)
+end
+
+local pairs52shadow = pairs53shadow
+local pairs52shadowest = pairs53shadowest
 
 local M = {}
 
-M.pairs51 = pairs51
-M.pairs52 = pairs52
-M.pairs53 = pairs53
+M.pairs51simple = pairs51simple
+M.pairs52simple = pairs52simple
+M.pairs53simple = pairs53simple
+
 M.native = _G.pairs
+
+M.pairs51shadow = pairs51shadow
+M.pairs51shadowest = pairs51shadowest
+
+M.pairs52shadow = pairs52shadow
+M.pairs52shadowest = pairs52shadowest
+
+M.pairs53shadow = pairs53shadow
+M.pairs53shadowest = pairs53shadowest
 
 return M
